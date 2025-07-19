ed232f1378a24e99f35a81de9551c2521186a3e2
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-07-19 14:11:20 -0300

    feat: add service httpClient globally

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: { preset: Lara, options: { darkModeSelector: '.dark-side' } },
     }),
+    provideHttpClient(),
   ],
 };
