be512092475d4b8a1909a3485fece49c53a1113d
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-08-05 19:27:33 -0300

    feat: defining type on get recipes

--- src/app/recipe/data/recipe.service.ts
+++ src/app/recipe/data/recipe.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+import { Recipe } from '../model/recipe.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
   readonly http = inject(HttpClient);
-
   readonly apiBaseUrl = environment.apiBaseUrl;
 
-  public getRecipes(): Observable<Object> {
-    return this.http.get(`${this.apiBaseUrl}/api/v1/recipes`);
+  public getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiBaseUrl}/api/v1/recipes`);
   }
 }
